ca0a88afaf6e21c9513f929653dd26b032c65c4f
EarthBet <55282774+EarthBet@users.noreply.github.com>
2020-01-07 08:27:50 -0500

    Commented on CommonProxy.java

--- main/java/com/example/examplemod/CommonProxy.java
+++ main/java/com/example/examplemod/CommonProxy.java
@@ -1,3 +1,4 @@
+//Do not edit anything above line 22
 package com.example.examplemod;
 
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
@@ -11,6 +12,11 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 @Mod.EventBusSubscriber
 public class CommonProxy {
 
+//Format for the following code chunk
+//Note: Do not edit anything before { or after }
+//registerRender(ExampleMod.itemName);
+//DO NOT CHANGE ANYTHING OTHER THAN THIS!
+//Just change the itemNames to your itemNames.
 	@SubscribeEvent
 	public static void registerRenders(ModelRegistryEvent event) {
 		registerRender(ExampleMod.mySword);
@@ -20,11 +26,15 @@ public class CommonProxy {
 		registerRender(ExampleMod.thatPickaxe);
 		registerRender(ExampleMod.thatSpade);
 	}
-	
+	//Do not edit this code chunk
 	private static void registerRender(Item item) {
 		ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation( item.getRegistryName(), "inventory"));
 	}
-	
+//Format for the following code chunk
+//Note: Do not edit anything before { or after }
+//event.getRegistry().registerAll(ExampleMod.itemName);
+//DO NOT CHANGE ANYTHING OTHER THAN THIS!
+//Just change the itemNames to your itemNames.
 	@SubscribeEvent
 	public static void registerItems(RegistryEvent.Register<Item> event) {
 		event.getRegistry().registerAll(ExampleMod.mySword);
